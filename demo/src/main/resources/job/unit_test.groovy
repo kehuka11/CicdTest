18a18310aa46f48f6e06a98db5d0a5ae7778edfb
maru3115 <yuya.marukawa3115@gmail.com>
2024-05-31 16:00:50 +0900

    jenkins file修正

--- demo/src/main/resources/job/unit_test.groovy
+++ demo/src/main/resources/job/unit_test.groovy
@@ -1,13 +1,19 @@
 pipeline {
     agent any
+    environment {
+        container_count = 0
+        container_health = 0
+    }
 
     stages {
+
+
         stage('Checkout') {
             steps {
                 script {
                     try {
                         def branch = env.BRANCH_NAME
-                        checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: '']]])
+                        checkout([$class: 'GitSCM', branches: [[name: "*/${params.branch}"]], userRemoteConfigs: [[url: '/home/repo']]])
                     } catch(Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("Checkout failed: ${e.message}")
@@ -16,13 +22,30 @@ pipeline {
             }
         }
 
-        stage('Start Docker') {
+         stage('Create Docker network') {
+             steps {
+                 script {
+                     try {
+                         sh 'docker network create cicd_test'
+                     }catch(Exception e) {
+                         currentBuild.result = 'FAILURE'
+                         error("docker-network failed: ${e.message}")
+                     }
+                 }
+             }
+         }
+
+        stage('Start Container') {
             steps {
                 script {
                     try {
-                        sh 'docker-compose up -d'
+                        sh 'cd ./demo; ls -la; PATH=$PATH:/usr/local/bin/docker-compose; docker-compose up -d'
 
-                        sleep(time:120, unit: 'SECONDS')
+                        while(container_count == container_health) {
+                            sleep(time:60, unit: 'SECONDS')
+                            env.result = sh 'docker ps | grep healthy'
+                            container_health = env.result
+                        }
                     } catch(Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("docker-compose failed: ${e.message}")
@@ -35,10 +58,38 @@ pipeline {
             steps {
                 script {
                     try {
-                        sh './gradlew flywayMigrate'
+                        sh 'cd ./demo;  chmod +x ./gradlew; ./gradlew flywayMigrate'
                     } catch(Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("Flyway Migrate failed: ${e.message}")
+                    }
+                }
+            }
+        }
+
+        stage('Unit Test') {
+            steps {
+                script {
+                    try {
+                        sh 'cd ./demo; chmod +x ./gradlew; ./gradlew test'
+                    } catch(Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("test failed: ${e.message}")
+                    }
+                }
+            }
+        }
+
+        stage('Down Container') {
+            steps {
+                script {
+                    try {
+                        sh 'cd ./demo; docker-compose up -d'
+
+
+                    } catch(Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("docker-compose failed: ${e.message}")
                     }
                 }
             }
